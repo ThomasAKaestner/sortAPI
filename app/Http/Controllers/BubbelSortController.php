60e63e3152bc318fead153665a1b99004e5380a3
Thomas Kaestner <t.kaestner@gamespipe.com>
2020-01-16 17:31:08 +0100

    added all

---
+++ app/Http/Controllers/BubbelSortController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class BubbleSortController extends Controller
+{
+    public function post()
+    {
+
+
+
+    }
+
+
+
+    private function bubbelSort()
+    {
+        
+    }
+}
---
+++ app/Http/Controllers/SortController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class SortController extends Controller
+{
+    public function get()
+    {
+        return "<h1>Sort API</h1><br>
+        <ul>
+            <li>Bubbel Sort API: http://localhost:8080/sort/bubble</li>
+            <li>Selection Sort API:http://localhost:8080/sort/selection</li>
+            <li>Insertion Sort API: http://localhost:8080/sort/insertion</li>
+            <li>Merge Sort API:http://localhost:8080/sort/merge</li>
+        </ul>
+        ";
+    }
+}
