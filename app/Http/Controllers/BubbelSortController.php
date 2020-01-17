7412b9937fc64b67afea0dd204fc771cf79b2526
Thomas Kaestner <t.kaestner@gamespipe.com>
2020-01-17 10:58:47 +0100

    -changed read.me -added quicksort

--- app/Http/Controllers/BubbelSortController.php
+++ app/Http/Controllers/BubbelSortController.php
@@ -1,20 +1,42 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
 
-class BubbleSortController extends Controller
+class BubbelSortController extends Controller
 {
-    public function post()
+    public function post(Request $request)
     {
-
-
-
+        $list = $request->input("list");
+        $list = json_decode($list);
+        dd($this->bubbelSort($list));
     }
 
-
-
-    private function bubbelSort()
-    {
-        
+    private function bubbelSort(array $list)
+    {   
+        $comparisons= 0;
+        do
+        {
+            $swapped = false;
+            for($i = 0; $i < sizeOf($list)-1; $i++)
+            {
+                if($list[$i] > $list[$i+1] && isset($list[$i+1]))
+                {
+                    $comparisons++;
+                    $temp = $list[$i];
+                    $list[$i] = $list[$i+1];
+                    $list[$i+1] = $temp;
+                    $swapped = true;
+                }
+            
+            }
+            
+        }
+        while($swapped);
+        $result = [
+            "comparisons" => $comparisons,
+            "list" => $list
+        ];
+        return $result;
     }
 }
